92d903d747c993bd3565f248162746a2457bcf93
unknown <ovchinnicov132@gmail.com>
2024-07-12 18:32:14 +0300

    Add polyfil to copy to buffer

--- src/pages/wallet.tsx
+++ src/pages/wallet.tsx
@@ -121,15 +121,54 @@ const Wallet = () => {
     localStorage.setItem('selectedWallet', wallet.publicKey);
   };
 
+  // const copyToClipboard = (text: string) => {
+  //   navigator.clipboard.writeText(text).then(
+  //     () => {
+  //       console.log('Copied to clipboard successfully!');
+  //     },
+  //     (err) => {
+  //       console.error('Could not copy text: ', err);
+  //     }
+  //   );
+  // };
+
   const copyToClipboard = (text: string) => {
+    if (!navigator.clipboard) {
+      fallbackCopyTextToClipboard(text);
+      return;
+    }
     navigator.clipboard.writeText(text).then(
       () => {
         console.log('Copied to clipboard successfully!');
       },
       (err) => {
         console.error('Could not copy text: ', err);
+        fallbackCopyTextToClipboard(text);
       }
     );
+  };
+
+  const fallbackCopyTextToClipboard = (text: string) => {
+    const textArea = document.createElement('textarea');
+    textArea.value = text;
+
+    textArea.style.top = '0';
+    textArea.style.left = '0';
+    textArea.style.position = 'fixed';
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+      const successful = document.execCommand('copy');
+      const msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Fallback: Copying text command was ' + msg);
+    } catch (err) {
+      console.error('Fallback: Oops, unable to copy', err);
+    }
+
+    document.body.removeChild(textArea);
   };
 
   const goToStore = () => {
